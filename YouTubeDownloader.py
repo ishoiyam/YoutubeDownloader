c711c03e1f6b482be798925148f84aea359480f9
Isho Ahmed <65296326+ishoiyam@users.noreply.github.com>
2020-08-07 18:32:06 +0100

    Update YouTubeDownloader.py

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -1,15 +1,37 @@
-import pafy 
+#!/usr/bin/env python3
+
+import pafy
 
 prompt = "\t>> "
 
+def vid(url):
+    video = pafy.new(url)
+    best = video.getbest()
+    best.download(filepath=".", quiet=False)
+
+
+def audio(url):
+    audio = pafy.new(url)
+    best = audio.getbestaudio()
+    best.download(filepath=".", quiet=False)
+
+
+def vid_pl(url):
+    playlist = pafy.get_playlist(url)
+    for i in playlist["item"]:
+        best_v = i["pafy"].getbest()
+        best_v.download(filepath=".", quiet=False)
+
+
 def main():
-    print("Welcome to YouTubeDownloder !!")
+    print("Welcome to Youtube Downloader!!")
 
     url = input("\n[+] Please enter your url: " + prompt)
     video = pafy.new(url)
     print(video.title)
-    
-    choice = input("\n[+] Download as: (vid | audio | vidPly\n" + prompt)
+
+
+    choice = input("\n[+] Download as: (vid | audio | vidPly)" + prompt)
 
     if choice == "vid":
         vid(url)
@@ -18,23 +40,8 @@ def main():
     elif choice == "vidPly":
         vid_pl(url)
     else:
-        print("[-] Please specify one the available options !!!")
+        print("[-] Please specify one of the avilable options !!")
 
+if __name__ == "__main__":
+    main()
 
-def vid(url):
-    video = pafy.new(url)
-    best = video.getbest()
-    best.download(filepath=".", quiet=False)
-
-def audio(url):
-    audio = pafy.new(url)
-    best = audio.getbestaudio()
-    best.download(filepath=".", quiet=False)
-
-def vid_pl(url):
-    playlist = pafy.get_playlist(url)
-    for i in playlist["items"]:
-        best_v = i["pafy"].getbest()
-        best_v.download(filepath=".", quiet=False)
-
-main()
